ebd923a4ee479a084b95ffafbe3a18f181569874
yas99en <yas99en@gmail.com>
2025-04-27 23:21:24 +0900

    springboot3

--- 05-spring-web-application-development/5.6/spring-webapp-form-implementation/src/main/java/com/example/app/AccountCreateForm1.java
+++ 05-spring-web-application-development/5.6/spring-webapp-form-implementation/src/main/java/com/example/app/AccountCreateForm1.java
@@ -3,8 +3,8 @@ package com.example.app;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
