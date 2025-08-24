2387166baeb0c6e8a0b5f9db639894b403fa465a
yas99en <yas99en@gmail.com>
2025-08-24 22:43:43 +0900

    updated

--- setup_db.sh
+++ setup_db.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-env PGCLIENTENCODING=UTF8 psql -U postgres $(find . -name setup_db.sql | sed -e 's|^| -f |')
+env PGCLIENTENCODING=UTF8 psql -U postgres -h 127.0.0.1 $(find . -name setup_db.sql | sed -e 's|^| -f |')
