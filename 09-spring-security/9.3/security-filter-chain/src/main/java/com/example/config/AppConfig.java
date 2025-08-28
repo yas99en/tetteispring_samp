7441a3be8fc686ff0fb4219da2f340da02768f6b
yas99en <yas99en@gmail.com>
2025-08-28 23:44:49 +0900

    spingboot3

--- 09-spring-security/9.3/security-filter-chain/src/main/java/com/example/config/AppConfig.java
+++ 09-spring-security/9.3/security-filter-chain/src/main/java/com/example/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import java.time.Duration;
+
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -37,7 +39,7 @@ public class AppConfig {
         dataSource.setMaxTotal(maxTotal);
         dataSource.setMaxIdle(maxIdle);
         dataSource.setMinIdle(minIdle);
-        dataSource.setMaxWaitMillis(maxWaitMillis);
+		dataSource.setMaxWait(Duration.ofMillis(maxWaitMillis));
         return dataSource;
     }
 
--- 09-spring-security/9.3/security-filter-chain/src/main/java/com/example/config/WebSecurityConfig.java
+++ 09-spring-security/9.3/security-filter-chain/src/main/java/com/example/config/WebSecurityConfig.java
@@ -1,18 +1,17 @@
 package com.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 import com.example.domain.service.account.AccountUserDetailsService;
 
@@ -31,7 +30,7 @@ public class WebSecurityConfig {
     @Bean
     @Order(1)
     public SecurityFilterChain uiSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.antMatcher("/ui/**")
+        http.securityMatcher("/ui/**")
         .formLogin(login -> login
                 .loginProcessingUrl("/ui/login") // ログインURL
                 .loginPage("/ui/login") // ログインフォームを表示するためのパス
@@ -48,11 +47,11 @@ public class WebSecurityConfig {
     @Order(2)
     public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
        http
-       	.antMatcher("/api/**")    
-       	.authorizeRequests(authorize -> authorize
+       	.securityMatcher("/api/**")    
+       	.authorizeHttpRequests(authorize -> authorize
     			.anyRequest().authenticated()
     	)
-		.httpBasic(withDefaults());
+		.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
