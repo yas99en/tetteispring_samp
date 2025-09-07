c6b5456ef20d6abd2b0908c0505a8fe12bfd3c92
yas99en <yas99en@gmail.com>
2025-09-08 02:04:56 +0900

    springboot3

--- 09-spring-security/9.9/spring-security-test/src/main/java/com/example/app/AccountForm.java
+++ 09-spring-security/9.9/spring-security-test/src/main/java/com/example/app/AccountForm.java
@@ -2,9 +2,9 @@ package com.example.app;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class AccountForm implements Serializable { 
 	private static final long serialVersionUID = 1L;
--- 09-spring-security/9.9/spring-security-test/src/main/java/com/example/config/WebSecurityConfig.java
+++ 09-spring-security/9.9/spring-security-test/src/main/java/com/example/config/WebSecurityConfig.java
@@ -1,31 +1,31 @@
 package com.example.config;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.authorization.AuthorizationDecision;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.IpAddressMatcher;
-
-import com.example.domain.service.account.AccountUserDetailsService;
 
 @Configuration 
 @EnableWebSecurity
 @Import({WebMvcConfig.class, AppConfig.class})
 public class WebSecurityConfig {
 	
-	private AccountUserDetailsService accountUserDetailsService;
+//	private AccountUserDetailsService accountUserDetailsService;
+//
+//	public WebSecurityConfig(AccountUserDetailsService accountUserDetailsService) {
+//		this.accountUserDetailsService = accountUserDetailsService;
+//	}
 
-	public WebSecurityConfig(AccountUserDetailsService accountUserDetailsService) {
-		this.accountUserDetailsService = accountUserDetailsService;
+	public WebSecurityConfig() {
 	}
 	
     @Bean
@@ -39,11 +39,11 @@ public class WebSecurityConfig {
         ).logout(logout -> logout
                 .logoutSuccessUrl("/logout").permitAll()
         ).authorizeHttpRequests(authz -> authz
-                .antMatchers("/accounts/**").permitAll()
-                .antMatchers("/").permitAll()
-                .antMatchers("/general/**").hasRole("GENERAL")
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/admin/**").access((authentication, context) -> {
+                .requestMatchers(new AntPathRequestMatcher("/accounts/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/general/**")).hasRole("GENERAL")
+                .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN")
+                .requestMatchers(new AntPathRequestMatcher("/admin/**")).access((authentication, context) -> {
                 	IpAddressMatcher ipAddressMatcher = new IpAddressMatcher("127.0.0.1");
                 	HttpServletRequest request = context.getRequest();
                 	return new AuthorizationDecision(ipAddressMatcher.matches(request));
@@ -56,11 +56,12 @@ public class WebSecurityConfig {
         
         return http.build();
     }
-    
-	@Autowired
-	public void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(accountUserDetailsService).passwordEncoder(passwordEncoder());
-	}
+
+//  springboot3 では以下は不要    
+//	@Autowired
+//	public void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(accountUserDetailsService).passwordEncoder(passwordEncoder());
+//	}
     
     @Bean
     public PasswordEncoder passwordEncoder() {
