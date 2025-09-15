e22b0859a6c600b8d8d3db285c877d80e6da7df0
yas99en <yas99en@gmail.com>
2025-09-15 11:07:21 +0900

    change javax.annotation to jakarta.annotation

--- 06-restful-web-service-development/6.5/handle-various-exceptions/src/main/java/com/example/app/domain/service/book/BookServiceImpl.java
+++ 06-restful-web-service-development/6.5/handle-various-exceptions/src/main/java/com/example/app/domain/service/book/BookServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.stereotype.Service;
 
