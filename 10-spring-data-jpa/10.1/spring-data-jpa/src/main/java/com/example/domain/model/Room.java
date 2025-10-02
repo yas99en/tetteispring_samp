907488b01fcea21f3af7e57f193ccf646c84d6ca
yas99en <yas99en@gmail.com>
2025-10-03 00:22:37 +0900

    springboot3

--- 10-spring-data-jpa/10.1/spring-data-jpa/src/main/java/com/example/domain/model/Room.java
+++ 10-spring-data-jpa/10.1/spring-data-jpa/src/main/java/com/example/domain/model/Room.java
@@ -3,14 +3,14 @@ package com.example.domain.model;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 // 10.1.2. Entity roomテーブルに対するEntityクラスの定義
 // 10.1.5. 関連 双方向の1対多/多対1の関連を両Entityへ設定
--- 10-spring-data-jpa/10.1/spring-data-jpa/src/main/java/com/example/domain/service/RoomServiceImpl.java
+++ 10-spring-data-jpa/10.1/spring-data-jpa/src/main/java/com/example/domain/service/RoomServiceImpl.java
@@ -2,9 +2,9 @@ package com.example.domain.service;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
