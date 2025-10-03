983ad59f9273d0871a09bbbf4e031881592c6413
yas99en <yas99en@gmail.com>
2025-10-03 17:38:19 +0900

    springboot3

--- 10-spring-data-jpa/10.5/spring-data-jpa-setup/src/main/java/com/example/domain/model/Room.java
+++ 10-spring-data-jpa/10.5/spring-data-jpa-setup/src/main/java/com/example/domain/model/Room.java
@@ -3,12 +3,12 @@ package com.example.domain.model;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "room")
