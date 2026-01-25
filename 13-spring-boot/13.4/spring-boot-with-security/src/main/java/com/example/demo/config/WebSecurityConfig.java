f8c15d11ed91c59af39e3d09f77343d67996939a
yas99en <yas99en@gmail.com>
2026-01-25 17:52:42 +0900

    springboot3

--- 13-spring-boot/13.4/spring-boot-with-security/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ 13-spring-boot/13.4/spring-boot-with-security/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,33 +1,31 @@
 package com.example.demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.example.demo.domain.service.account.AccountUserDetailsService;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 
 // 13.4.2. 認証・認可のカスタマイズ フォーム認証を適用するためのBean定義例
 @Configuration
 public class WebSecurityConfig {
 
-    private AccountUserDetailsService accountUserDetailsService;
-
-    @Autowired
-    public WebSecurityConfig(AccountUserDetailsService accountUserDetailsService) {
-        this.accountUserDetailsService = accountUserDetailsService;
-    }
+//    private AccountUserDetailsService accountUserDetailsService;
+//
+//    @Autowired
+//    public WebSecurityConfig(AccountUserDetailsService accountUserDetailsService) {
+//        this.accountUserDetailsService = accountUserDetailsService;
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(requests -> requests
-                .antMatchers("/", "/home").permitAll()
+           		.requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/")).permitAll()
+           		.requestMatchers(PathPatternRequestMatcher.withDefaults().matcher("/home")).permitAll()
                 .anyRequest().authenticated()
             )
             .formLogin(form -> form
@@ -39,11 +37,12 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-	// 13.4.2. 認証・認可のカスタマイズ 自作のUserDetailsServiceを適用するためのBean定義例
-    @Autowired
-    public void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
-    	auth.userDetailsService(accountUserDetailsService);
-    }
+// SpringBoot3では不要
+//	// 13.4.2. 認証・認可のカスタマイズ 自作のUserDetailsServiceを適用するためのBean定義例
+//    @Autowired
+//    public void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+//    	auth.userDetailsService(accountUserDetailsService);
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
