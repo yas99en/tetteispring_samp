5bdd0abe9858dfa453872a93c3ac5777444beb14
yas99en <yas99en@gmail.com>
2026-01-25 23:43:01 +0900

    springboot3

--- 13-spring-boot/13.6/spring-boot-actuator/src/main/java/com/example/demo/SecurityConfiguration.java
+++ 13-spring-boot/13.6/spring-boot-actuator/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,16 +15,16 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain actuatorSecurityFilter(HttpSecurity http) throws Exception {
 		http
-			.requestMatcher(EndpointRequest.toAnyEndpoint())
-			.authorizeRequests(requests -> requests
+			.securityMatcher(EndpointRequest.toAnyEndpoint())
+			.authorizeHttpRequests(requests -> requests
 				// 13.6.4.の動作確認を実施する場合はhasRole行のコメントアウトを外し、permitAll行をコメントアウトしてください
-//				.anyRequest().hasRole("ENDPOINT_ADMIN")
-				.anyRequest().permitAll()
+				.anyRequest().hasRole("ENDPOINT_ADMIN")
+//				.anyRequest().permitAll()
 			)
 			// 13.6.1.のshutdown（POST）の動作確認用にCSRF保護を無効化
 			// Spring Securityを有効にした場合、POSTメソッド実行時にCSRFトークンチェックが実行されてエラーとなるため
 			.csrf(csrf -> csrf.disable())
-			.httpBasic();
+			.httpBasic(Customizer.withDefaults());
 		return http.build();
 	}
 	
