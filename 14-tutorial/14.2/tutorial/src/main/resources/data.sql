9c82fab66d8a9c35a3eeb23fd9ec0d077cac1148
yas99en <yas99en@gmail.com>
2026-01-28 22:25:14 +0900

    added comment

--- 14-tutorial/14.2/tutorial/src/main/resources/data.sql
+++ 14-tutorial/14.2/tutorial/src/main/resources/data.sql
@@ -1,4 +1,5 @@
 -- 14.2.5.5. Reservation PL/pgSQL記述例
+-- パスワードはすべてdemo
 INSERT INTO usr (user_id, first_name, last_name, role_name, password)
 VALUES ('taro-yamada', '太郎', '山田', 'USER', '$2a$10$oxSJl.keBwxmsMLkcT9lPeAIxfNTPNQxpeywMrF7A3kVszwUTqfTK')/;
 INSERT INTO usr (user_id, first_name, last_name, role_name, password)
